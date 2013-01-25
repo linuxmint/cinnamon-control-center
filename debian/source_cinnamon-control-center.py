ab72093751521eff1f9ff301411a05cc27e50473
Michael Webster <miketwebster@gmail.com>
2013-01-25 11:44:05 -0500

    More renaming

--- debian/source_cinnamon-control-center.py
+++ debian/source_cinnamon-control-center.py
@@ -2,11 +2,11 @@ import os, apport.packaging, re
 from apport.hookutils import *
 
 def add_info(report):
-	# the issue is not in the gnome-control-center code so reassign
-	if "Stacktrace" in report and "/usr/lib/control-center-1" in report["Stacktrace"]:
+	# the issue is not in the cinnamon-control-center code so reassign
+	if "Stacktrace" in report and "/usr/lib/cinnamon-control-center-1" in report["Stacktrace"]:
 		for words in report["Stacktrace"].split():
-			if words.startswith("/usr/lib/control-center-1"):
-			    if apport.packaging.get_file_package(words) != 'gnome-control-center':
+			if words.startswith("/usr/lib/cinnamon-control-center-1"):
+			    if apport.packaging.get_file_package(words) != 'cinnamon-control-center':
     				report.add_package_info(apport.packaging.get_file_package(words))
     				return    				
 			    component = re.compile("lib(\w*).so").search(words).groups(1)[0]
@@ -16,14 +16,14 @@ def add_info(report):
 
 	# collect informations on the /usr/lib/control-center-1 components 
 	plugin_packages = set()
-	for dirpath, dirnames, filenames in os.walk("/usr/lib/control-center-1"):
+	for dirpath, dirnames, filenames in os.walk("/usr/lib/cinnamon-control-center-1"):
 		for filename in filenames:
 			path = os.path.join(dirpath, filename)
 			package = apport.packaging.get_file_package(path)
-			if package == 'gnome-control-center':
+			if package == 'cinnamon-control-center':
 				continue
 
 			plugin_packages.add(package)
 
-	report["usr_lib_gnome-control-center"] = package_versions(*sorted(plugin_packages))
+	report["usr_lib_cinnamon-control-center"] = package_versions(*sorted(plugin_packages))
 
