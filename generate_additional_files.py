8111461df1f66bb78bbd45a975897801b93bccb1
Clement Lefebvre <clement.lefebvre@linuxmint.com>
2022-07-04 09:38:46 +0200

    Fix desktop files

--- generate_additional_files.py
+++ generate_additional_files.py
@@ -14,7 +14,12 @@ Exec=cinnamon-settings color
 Icon=cs-color
 Terminal=false
 Type=Application
+StartupNotify=true
+Categories=GTK;Settings;X-Cinnamon-Settings-Panel;HardwareSettings
+OnlyShowIn=X-Cinnamon;
 X-Cinnamon-Settings-Panel=color
+# Translators: those are keywords for the color control-center panel
+_Keywords=Color;ICC;Profile;Calibrate;Printer;Display;
 """
 
 additionalfiles.generate(DOMAIN, PATH, "./panels/color/cinnamon-color-panel.desktop", prefix, _("Color"), _("Color management settings"), "")
@@ -24,27 +29,43 @@ Exec=cinnamon-settings region
 Icon=cs-language
 Terminal=false
 Type=Application
+StartupNotify=true
+Categories=GTK;Settings;DesktopSettings;X-Cinnamon-Settings-Panel;X-Cinnamon-PersonalSettings
+OnlyShowIn=X-Cinnamon;
 X-Cinnamon-Settings-Panel=region
+# Translators: those are keywords for the region control-center panel
+_Keywords=Language;Layout;Keyboard;
+NoDisplay=true
 """
 
 additionalfiles.generate(DOMAIN, PATH, "./panels/region/cinnamon-region-panel.desktop", prefix, _("Region & Language"), _("Change your region and language settings"), "")
 
 prefix = """[Desktop Entry]
-Exec=cinnamon-control-center display
+Exec=cinnamon-settings display
 Icon=cs-display
 Terminal=false
 Type=Application
+StartupNotify=true
+Categories=GTK;Settings;HardwareSettings;X-Cinnamon-Settings-Panel;
+OnlyShowIn=X-Cinnamon;
 X-Cinnamon-Settings-Panel=display
+# Translators: those are keywords for the display control-center panel
+_Keywords=Panel;Projector;xrandr;Screen;Resolution;Refresh;
 """
 
 additionalfiles.generate(DOMAIN, PATH, "./panels/display/cinnamon-display-panel.desktop", prefix, _("Display"), _("Change resolution and position of monitors and projectors"), "")
 
 prefix = """[Desktop Entry]
-Exec=cinnamon-control-center network
+Exec=cinnamon-settings network
 Icon=cs-network
 Terminal=false
 Type=Application
+StartupNotify=true
+Categories=GTK;Settings;HardwareSettings;X-Cinnamon-Settings-Panel;
+OnlyShowIn=X-Cinnamon;
 X-Cinnamon-Settings-Panel=network
+# Translators: those are keywords for the network control-center panel
+_Keywords=Network;Wireless;IP;LAN;Proxy;Internet;WiFi;
 """
 
 additionalfiles.generate(DOMAIN, PATH, "./panels/network/cinnamon-network-panel.desktop", prefix, _("Network"), _("Network settings"), "")
@@ -54,7 +75,12 @@ Exec=cinnamon-settings wacom
 Icon=cs-tablet
 Terminal=false
 Type=Application
+StartupNotify=true
+Categories=GNOME;GTK;Settings;HardwareSettings;X-Cinnamon-Settings-Panel;
 X-Cinnamon-Settings-Panel=wacom
+OnlyShowIn=X-Cinnamon;
+# Translators: those are keywords for the wacom tablet control-center panel
+_Keywords=Tablet;Wacom;Stylus;Eraser;Mouse;
 """
 
 additionalfiles.generate(DOMAIN, PATH, "./panels/wacom/cinnamon-wacom-panel.desktop", prefix, _("Graphics Tablet"), _("Set button mappings and adjust stylus sensitivity for graphics tablets"), "")
